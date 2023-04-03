ed77c3cbed848630700595018e89773f62c3440f
tomigigee <tomigigee@gmail.com>
2023-04-03 10:29:17 -0300

    3/4

---
+++ practicas/practica_introduccion_python2.py
@@ -0,0 +1,48 @@
+
+#1)Definir un procedimiento que tome como parámetro una lista, verifique si tiene el elemento "control" y le agregue el string "revisado" y le quite el elemento "control".
+
+
+#2)Definir un procedimiento que tome una lista como parámetro y elimine el primer elemento de la lista. Hacer las verificaciones que sean necesarias.
+
+ 
+#3)Definir una función que dada una lista y un elemento devuelva la posición de este elemento en la lista.
+
+ 
+#4)Definir un procedimiento que tome como parámetros dos listas y un elemento, en la que se debe eliminar el elemento de una lista y agregarlo a la otra. Realizar dos versiones del , usando un método distinto para eliminar el elemento en cada versión. ¿Qué problema/s tiene este procedimiento?
+
+ 
+#5)Escribir una función que tome como parámetro una lista de enteros y devuelva una lista con valores booleanos que indique si cada elemento es par o impar. Por ejemplo, para la lista [2, 45, 108, 12, 7], la función debería retornar [True, False, True, True, False]. Utilizar la función realizada en la práctica anterior.
+
+ 
+#6)Escribir una función que lea un string y devuelva un diccionario con la cantidad de apariciones de cada carácter en la cadena (considerar que las mayúsculas difieren de las minúsculas, por lo que, si el string es "Agua", el carácter "A" tiene 1 aparición y el carácter "a" también tiene 1).
+
+ 
+#7)Modificá la función anterior para que además imprima los caracteres que no aparecen en la cadena, pero con el valor 0.
+
+ 
+#8)Definir una función que dada una palabra, nos diga si el palíndromo o no.
+
+ 
+#9)Definir una función llamada productoria que toma como parámetro una lista de números y los multiplica a todos.
+
+
+#10)Creá un programa para gestionar datos de los socios de un club, el cual permita: 
+#Cargar la información de los socios en un diccionario, al cual poder acceder por número de socio. Los datos que se deben almacenar son: número, nombre y apellido, fecha de ingreso (ddmmaaaa), cuota al dia (s/n). El programa debe iniciar con los datos de los socios fundadores ya cargados, los cuales son:
+
+#Socio número 1, Florencia Ocampo, ingresó el 14/09/2001, cuota al día
+
+#Socio número 2, David Estévez, ingresó el 14/09/2001, cuota al día
+
+#Socio número 3, Sofía Cáceres, ingresó el 14/09/2001, cuota al día
+
+#Informar la cantidad de socios que tiene el club.
+
+#Solicitar al usuario el número de un socio y registrar que ha pagado todas las cuotas.
+
+#Modificar la fecha de ingreso de todos los socios ingresados el 21/10/2017, indicando que en realidad ingresaron el 22/10/2017.
+
+#Solicitar el nombre y apellido d eun socio y darlo de baja (eliminarlo del listado).
+
+#Imprimir el listado de socios completos.
+
+#Definir las funciones y/o procedimientos que creas necesarios.
